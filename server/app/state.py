120fa9fcb76b5a8a8c5467b129ee9c4553c7023a
Karen Bie <karen_bie@hotmail.com>
2019-09-15 04:14:09 -0400

    Temporary fix for similar_customer_cache global.

--- server/app/state.py
+++ server/app/state.py
@@ -8,6 +8,7 @@ similar_customers_cache = []
 
 def update_similar_customers_cache():
     from app.td import db
+    global similar_customers_cache
     similar_customers_cache = db.get_similar_customers(age=_current_customer['age'], municipality=_current_customer['municipality'], totalIncome=_current_customer['totalIncome'])
 
 def set_current_customer(data):
