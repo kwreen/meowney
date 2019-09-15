9acc471306a2dc73b4252fbe694a071a038892e6
Simon Huang <simon@simonhuang.ca>
2019-09-15 04:23:57 -0400

    Progress on API calls and added graphs

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # Needed to send/receive http requests instead of https to localhost
 
 
 from app import routes
